b89c608d0c3265120017ad75bf794caac2f08e12
Norman Saez <nfsaez@uc.cl>
2013-01-14 16:39:17 -0300

    Adding filters to gross file

--- scripts/grossSH.py
+++ scripts/grossSH.py
@@ -1,5 +1,12 @@
 import FITS
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from scipy import ndimage
+
+
+
+
 
 data = FITS.Read('myfits.fits')
 
@@ -9,25 +16,56 @@ data = FITS.Read('myfits.fits')
 headers = data[0]
 raw_data = data[1]
 
-#print headers['parsed']
-#y_axis = int(headers['parsed']['NAXIS2'])
-#x_axis = int(headers['parsed']['NAXIS1'])
 
+#gettting stats
 average = np.mean(raw_data)
 maxi = raw_data.max()
 mini = raw_data.min()
+(y,x) = raw_data.shape
+cloned = np.zeros(raw_data.shape)
 
 #reshape if is necesarry:
-print raw_data.size
-print raw_data.ndim
+raw_data = np.reshape(raw_data, raw_data.size)
+cloned = np.reshape(cloned,cloned.size)
+
+################################################
+count = 0
+threshole = average*7.0
+print threshole
+for i in raw_data:
+    if i > threshole:
+        cloned[count] = 1
+    count = count + 1
+
+################################################
 print raw_data.shape
-cloned = np.zeros(raw_data.shape)
-#raw_data = np.reshape(raw_data, total_size)
-#
-#count = 0
-#for i in cloned:
-#    if i > average:
-#        cloned_b(count) = 1
-#    else:
-#        cloned_b(count) = 0
-#    count = count + 1 
+print cloned.shape
+print x
+print y
+#cloned = cloned.reshape(x,y)
+#plt.imshow(cloned)
+#plt.gca().invert_yaxis()
+#plt.show()
+FITS.Write(cloned,'myfitsBW.fits')
+print "writed"
+##################################################
+#im = scipy.misc.imread('myfits.fits')
+print type(raw_data)
+print raw_data.shape
+print raw_data.dtype
+#raw_1 = raw.astype(np.uint8)
+###########
+raw_data = raw_data.reshape(y,x)
+
+#processed = ndimage.sobel(raw_data, cval=0.9)
+#plt.imshow(processed)
+#plt.gca().invert_yaxis()
+#plt.show()
+#scipy.misc.imsave('sobel.jpg', processed)
+processed = ndimage.gaussian_laplace(raw_data, cval=0.1)
+#plt.imshow(processed)
+#plt.gca().invert_yaxis()
+#plt.show()
+scipy.misc.imsave('laplace.jpg', processed)
+
+print "finish"
