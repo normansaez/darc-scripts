d86d78552abb1ea112e881ffa665b9c14709ab5d
Norman Saez <nfsaez@uc.cl>
2013-01-14 16:54:42 -0300

    Removing useless attempts

--- scripts/grossSH.py
+++ scripts/grossSH.py
@@ -15,57 +15,8 @@ data = FITS.Read('myfits.fits')
 # list[1] = data (numpy.array)
 headers = data[0]
 raw_data = data[1]
-
-
-#gettting stats
-average = np.mean(raw_data)
-maxi = raw_data.max()
-mini = raw_data.min()
-(y,x) = raw_data.shape
-cloned = np.zeros(raw_data.shape)
-
-#reshape if is necesarry:
-raw_data = np.reshape(raw_data, raw_data.size)
-cloned = np.reshape(cloned,cloned.size)
-
-################################################
-count = 0
-threshole = average*7.0
-print threshole
-for i in raw_data:
-    if i > threshole:
-        cloned[count] = 1
-    count = count + 1
-
-################################################
-print raw_data.shape
-print cloned.shape
-print x
-print y
-#cloned = cloned.reshape(x,y)
-#plt.imshow(cloned)
-#plt.gca().invert_yaxis()
-#plt.show()
-FITS.Write(cloned,'myfitsBW.fits')
-print "writed"
-##################################################
-#im = scipy.misc.imread('myfits.fits')
-print type(raw_data)
-print raw_data.shape
-print raw_data.dtype
-#raw_1 = raw.astype(np.uint8)
-###########
-raw_data = raw_data.reshape(y,x)
-
 processed = ndimage.sobel(raw_data, cval=1)
 plt.imshow(processed)
 plt.gca().invert_yaxis()
 plt.show()
 scipy.misc.imsave('sobel.jpg', processed)
-#processed = ndimage.laplace(raw_data, 0)
-#plt.imshow(processed)
-#plt.gca().invert_yaxis()
-#plt.show()
-#scipy.misc.imsave('laplace.jpg', processed)
-
-print "finish"
