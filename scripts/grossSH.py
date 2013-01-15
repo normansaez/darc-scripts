8145bee283372a4efa7e169cd7de7f5217b59907
Norman Saez <nfsaez@uc.cl>
2013-01-15 17:45:09 -0300

    Adding grossSH modifications

--- scripts/grossSH.py
+++ scripts/grossSH.py
@@ -5,6 +5,10 @@ import scipy.misc as  misc
 from scipy import ndimage
 import matplotlib.cm as cm
 import pylab
+from skimage.draw import ellipse
+from skimage.morphology import label
+from skimage.measure import regionprops
+from scipy.ndimage import geometric_transform
 
 def im2bw(image, threshold):
     (x, y) = image.shape
@@ -27,25 +31,42 @@ headers = data[0]
 raw_data = data[1]
 
 #plot
-plt.figure(1)
+#plt.figure(1)
 #plt.subplot(131)
-plt.imshow(raw_data, cmap=pylab.gray())
-plt.gca().invert_yaxis()
+#plt.gca().invert_yaxis()
+#plt.imshow(raw_data, cmap=pylab.gray())
 
 raw_data = im2bw(raw_data,0.4)
-plt.figure(2)
+#plot
+#plt.figure(2)
 #plt.subplot(132)
-plt.imshow(raw_data, cmap=pylab.gray())
-plt.gca().invert_yaxis()
-#print raw_data
-#print raw_data.max()
-#print raw_data.min()
+#plt.gca().invert_yaxis()
+#plt.imshow(raw_data, cmap=pylab.gray())
 processed = ndimage.filters.laplace(raw_data,mode='reflect', cval=0.0)
+raw_data = processed
+# plot
+#plt.figure(3)
+#plt.subplot(133)
+#plt.gca().invert_yaxis()
+#plt.imshow(processed, cmap=pylab.gray())
+#plt.show()
+
+label_img = label(processed)
+print label_img
 # plot
 plt.figure(3)
-#plt.subplot(133)
-plt.imshow(processed, cmap=pylab.gray())
+plt.subplot(133)
 plt.gca().invert_yaxis()
+plt.imshow(label_img, cmap=pylab.gray())
 plt.show()
+props = regionprops(label_img, ['BoundingBox','Centroid','Orientation','MajorAxisLength','MinorAxisLength'])
+for i in props:
+    x0 = i['Centroid'][0]
+    y0 = i['Centroid'][0]
+    plt.plot(x0, y0, '.g', markersize=15)
+
+print len(props)
+plt.show()
+
 #save it!
-misc.imsave('laplace.jpg', processed)
+#misc.imsave('laplace.jpg', processed)
