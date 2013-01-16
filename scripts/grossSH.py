e4ece6a1b2f2cc31b134241660eca12557b87ee7
Norman Saez <nfsaez@uc.cl>
2013-01-16 17:57:26 -0300

    Fix subap centroid

--- scripts/grossSH.py
+++ scripts/grossSH.py
@@ -24,26 +24,26 @@ def im2bw(image, threshold):
 
 
 data = FITS.Read('myfits.fits')
-#data fits  = list
-# list[0] = headers
-# list[1] = data (numpy.array)
 headers = data[0]
 raw_data = data[1]
-
+#print raw_data.mean()
+#print raw_data.max()
+#print raw_data.min()
 #plot
 #plt.figure(1)
 #plt.subplot(131)
 #plt.gca().invert_yaxis()
 #plt.imshow(raw_data, cmap=pylab.gray())
-
-raw_data = im2bw(raw_data,0.4)
+tr = 0.42
+print tr
+raw_data = im2bw(raw_data,tr)
 #plot
 #plt.figure(2)
 #plt.subplot(132)
 #plt.gca().invert_yaxis()
 #plt.imshow(raw_data, cmap=pylab.gray())
-processed = ndimage.filters.laplace(raw_data,mode='reflect', cval=0.0)
-raw_data = processed
+
+#processed = ndimage.filters.laplace(raw_data,mode='reflect', cval=0.0)
 # plot
 #plt.figure(3)
 #plt.subplot(133)
@@ -51,22 +51,22 @@ raw_data = processed
 #plt.imshow(processed, cmap=pylab.gray())
 #plt.show()
 
-label_img = label(processed)
-print label_img
+label_img = label(raw_data)
+#label_img = raw_data.astype(np.int)
+#print label_img
 # plot
-plt.figure(3)
-plt.subplot(133)
-plt.gca().invert_yaxis()
-plt.imshow(label_img, cmap=pylab.gray())
-plt.show()
-props = regionprops(label_img, ['BoundingBox','Centroid','Orientation','MajorAxisLength','MinorAxisLength'])
+#plt.figure(3)
+#plt.subplot(133)
+#plt.gca().invert_yaxis()
+#plt.imshow(label_img, cmap=pylab.gray())
+#plt.show()
+props = regionprops(label_img, ['Centroid'])
 for i in props:
     x0 = i['Centroid'][0]
-    y0 = i['Centroid'][0]
-    plt.plot(x0, y0, '.g', markersize=15)
-
+    y0 = i['Centroid'][1]
+#    plt.plot(x0, y0, 'xr', markersize=8)
 print len(props)
-plt.show()
+#plt.show()
 
 #save it!
 #misc.imsave('laplace.jpg', processed)
