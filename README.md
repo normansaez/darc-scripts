be1ecb7db330be8b29bb539ef5a2ff993fd7ed41
Norman Saez <nfsaez@uc.cl>
2013-01-17 18:08:58 -0300

    Updating README

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ darc-scripts
 ============
 
 Darc Scripts repo
+These scripts are made to be tested in UC, AO lab
+
+They are not a full project itself.
+I don't want to lost the history track
--- scripts/subapAlignmentCustom.py
+++ scripts/subapAlignmentCustom.py
@@ -1,21 +1,20 @@
+'''
+Gross SH Subap layout
+'''
 import FITS
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.misc as  misc
-from scipy import ndimage
-import matplotlib.cm as cm
-import pylab
-from skimage.draw import ellipse
 from skimage.morphology import label
 from skimage.measure import regionprops
-from scipy.ndimage import geometric_transform
 
 def im2bw(image, threshold):
-    (x, y) = image.shape
+    '''
+    Convert a numpy.array float image to binary image, given a threshold
+    '''
+    (axis_x, axis_y) = image.shape
     image = image/image.max()
-    img = np.zeros((x,y))
-    for i in range(0,x):
-        for j in range(0,y):
+    img = np.zeros((axis_x, axis_y))
+    for i in range(0, axis_x):
+        for j in range(0, axis_y):
             if image[i][j] >= threshold:
                 img[i][j] = 1
             else:
@@ -23,50 +22,17 @@ def im2bw(image, threshold):
     return img
 
 
+tr = 0.42
+print tr
+
 data = FITS.Read('myfits.fits')
 headers = data[0]
 raw_data = data[1]
-#print raw_data.mean()
-#print raw_data.max()
-#print raw_data.min()
-#plot
-#plt.figure(1)
-#plt.subplot(131)
-#plt.gca().invert_yaxis()
-#plt.imshow(raw_data, cmap=pylab.gray())
-tr = 0.42
-print tr
-raw_data = im2bw(raw_data,tr)
-#plot
-#plt.figure(2)
-#plt.subplot(132)
-#plt.gca().invert_yaxis()
-#plt.imshow(raw_data, cmap=pylab.gray())
 
-#processed = ndimage.filters.laplace(raw_data,mode='reflect', cval=0.0)
-# plot
-#plt.figure(3)
-#plt.subplot(133)
-#plt.gca().invert_yaxis()
-#plt.imshow(processed, cmap=pylab.gray())
-#plt.show()
-
+raw_data = im2bw(raw_data, tr)
 label_img = label(raw_data)
-#label_img = raw_data.astype(np.int)
-#print label_img
-# plot
-#plt.figure(3)
-#plt.subplot(133)
-#plt.gca().invert_yaxis()
-#plt.imshow(label_img, cmap=pylab.gray())
-#plt.show()
 props = regionprops(label_img, ['Centroid'])
-for i in props:
-    x0 = i['Centroid'][0]
-    y0 = i['Centroid'][1]
-#    plt.plot(x0, y0, 'xr', markersize=8)
-print len(props)
-#plt.show()
-
-#save it!
-#misc.imsave('laplace.jpg', processed)
+#for i in props:
+#    x0 = i['Centroid'][0]
+#    y0 = i['Centroid'][1]
+#print len(props)
