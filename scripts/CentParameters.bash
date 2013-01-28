39c95366ff8537ff94fd0211d168a16f1d616d0d
Norman Saez <nfsaez@uc.cl>
2013-01-28 16:17:06 -0300

    Show Parameter in order to debug Centroid stuff

---
+++ scripts/CentParameters.bash
@@ -0,0 +1,20 @@
+echo "darcmagic get averageCent --prefix=main"
+darcmagic get averageCent --prefix=main
+echo "darcmagic get centCalBounds --prefix=main"
+darcmagic get centCalBounds --prefix=main
+echo "darcmagic get centCalData --prefix=main"
+darcmagic get centCalData --prefix=main
+echo "darcmagic get centCalSteps --prefix=main"
+darcmagic get centCalSteps --prefix=main
+echo "darcmagic get centIndexArray --prefix=main"
+darcmagic get centIndexArray --prefix=main
+echo "darcmagic get centroidMode --prefix=main"
+darcmagic get centroidMode --prefix=main
+echo "darcmagic get centroidWeight --prefix=main"
+darcmagic get centroidWeight --prefix=main
+echo "darcmagic get recordCents --prefix=main"
+darcmagic get recordCents --prefix=main
+echo "darcmagic get refCentroids --prefix=main"
+darcmagic get refCentroids --prefix=main
+
+
