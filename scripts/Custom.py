2bda2be64b1898d6784ab096f454ac7d19a08810
Norman Saez <nfsaez@uc.cl>
2013-01-29 14:23:10 -0300

    Modifications and clean up variables names

--- scripts/Custom.py
+++ scripts/Custom.py
@@ -63,22 +63,14 @@ data = FITS.Read('myfits.fits')
 headers = data[0]
 raw_data = data[1]
 ###############################################
-print "Print FIT data"
-print raw_data
-print "Print FIT data"
-#npxlx =np.array(eval(data[0]["parsed"]["npxlx"]))
-#npxly =np.array(eval(data[0]["parsed"]["npxly"]))
-#nsub  =np.array(eval(data[0]["parsed"]["nsub"]))
-#print npxlx
-#print npxly
-#print nsub
-##############################################
 raw_data = im2bw(raw_data, tr)
 label_img = label(raw_data)
 props = regionprops(label_img, ['Centroid'])
 plt.figure(num=1, figsize=(8, 6), dpi=150, facecolor='w', edgecolor='k')
-new_subap = np.array([], "i")
-new_centroid = np.array([], dtype = np.float32)
+new_subapLocation = np.array([], "i")
+new_subapFlag = np.array([], "i")
+new_centroid = np.array([], "i")
+
 for i in props:
     x0 = i['Centroid'][0]
     y0 = i['Centroid'][1]
@@ -87,16 +79,10 @@ for i in props:
     x_end = x0 + side
     y_end = y0 + side
     
-    #x_end = x0 + side
-    #y_start = y0 - side
-
-    #x_start = x0 - side
-    #y_end = y0 + side
-
     x_start = x0 - side
     y_start = y0 - side
 
-    new_subap = np.append(new_subap,[[int(math.floor(y_start)),int(math.floor(y_end)),1,int(math.floor(x_start)),int(math.floor(x_end)),1]])
+    new_subapLocation = np.append(new_subapLocation,[[int(math.floor(y_start)),int(math.floor(y_end)),1,int(math.floor(x_start)),int(math.floor(x_end)),1]])
     plt.plot(y0,x0,'xr',markersize=8)
 
     plt.plot(y_end,x_end,'.y',markersize=2)
@@ -105,24 +91,27 @@ for i in props:
     plt.plot(y_start,x_start,'.y',markersize=2)
 plt.imshow(raw_data, cmap=pylab.gray())
 plt.gca().invert_yaxis()
+
+#get first point in subap centroid maps.
 mouse = MouseMonitor()
 connect('button_press_event', mouse.mycall)
 plt.show()
+
+print int(mouse.event.xdata)
+print int(mouse.event.ydata)
+#TODO this should be get from some other place !
 nsub = len(props)
 npxlx = 640
 npxly = 480
 
-new_subap = new_subap.reshape(new_subap.size/6,6)
+new_subapLocation = new_subapLocation.reshape(new_subapLocation.size/6,6)
 
-#plot([1,2,3])
-#show()
-
-print new_subap
+print new_subapLocation
 print nsub
 subflag = np.ones((nsub,),dtype=int)
-fname="newSubAp.fits"
+fname="newSubApLocation.fits"
 #{'END': '', 'npxly': '[480]', 'EXTEND': 'T', 'SIMPLE': 'T', 'NAXIS2': '182', 'NAXIS': '2', 'NAXIS1': '6', 'BITPIX': '32', 'npxlx': '[640]', 'nsub': '[182]'}
-FITS.Write(new_subap,fname,extraHeader=["npxlx   = '[%s]'"%str(npxlx),"npxly   = '[%s]'"%str(npxly),"nsub    = '[%s]'"%str(nsub)])
+FITS.Write(new_subapLocation,fname,extraHeader=["npxlx   = '[%s]'"%str(npxlx),"npxly   = '[%s]'"%str(npxly),"nsub    = '[%s]'"%str(nsub)])
 FITS.Write(subflag,fname,writeMode='a')
 #print new_ap.shape
 FITS.Write(new_centroid,'r_centroid.fits',extraHeader=["npxlx   = '[%s]'"%str(npxlx),"npxly   = '[%s]'"%str(npxly),"nsub    = '[%s]'"%str(nsub)])
