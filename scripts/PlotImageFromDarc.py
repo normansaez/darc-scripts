d271b00b2341839c2fafff750ed8cce6d666a711
Norman Saez <nfsaez@uc.cl>
2013-03-27 15:37:21 -0300

    cut off useless code

--- scripts/PlotImageFromDarc.py
+++ scripts/PlotImageFromDarc.py
@@ -1,30 +1,27 @@
-import os
-import sys
-import socket
-import time
-import glob
-import numpy
-import gtk
+#import os
+#import sys
+#import socket
+#import time
+#import glob
+#import numpy
+#import gtk
 import darc
-import FITS
-import correlation
-import calibrate
-import matplotlib
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
-import pylab
+#import FITS
+#import correlation
+#import calibrate
+#import matplotlib
+#from matplotlib.figure import Figure
+#from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
+#import pylab
+#import FITS
+import matplotlib.pyplot as plt
+#import pylab
+
 d=darc.Control("ShackHartmann")
 nfr = 100
 img=d.SumData("rtcCalPxlBuf",nfr)[0]/nfr
-print img
-img.size
-1920*1080
-img.size - 1920*1080
+print img.size - 1920*1080
 data = img.reshape(1080,1920)
-import FITS
-import matplotlib.pyplot as plt
-import pylab
-data
 plt.imshow(data)
 plt.gca().invert_yaxis()
 plt.show()
