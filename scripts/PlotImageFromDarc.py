b3f3ffc4f19cabbfa340f8e5c030487831d2b55b
Norman Saez <nfsaez@uc.cl>
2013-03-27 15:15:04 -0300

    Adding file to plot image

---
+++ scripts/PlotImageFromDarc.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import socket
+import time
+import glob
+import numpy
+import gtk
+import darc
+import FITS
+import correlation
+import calibrate
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
+import pylab
+d=darc.Control("ShackHartmann")
+nfr = 100
+img=d.SumData("rtcCalPxlBuf",nfr)[0]/nfr
+print img
+img.size
+1920*1080
+img.size - 1920*1080
+data = img.reshape(1080,1920)
+import FITS
+import matplotlib.pyplot as plt
+import pylab
+data
+plt.imshow(data)
+plt.gca().invert_yaxis()
+plt.show()
