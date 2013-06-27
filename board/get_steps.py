2c087848909b57737e9d15a7a9318a1915110ab4
Norman Saez <nsaez@alma.cl>
2013-06-27 01:59:38 -0400

    fixing movement

--- board/get_steps.py
+++ board/get_steps.py
@@ -1,48 +1,43 @@
 import logging
 
+CAMBIO = {"IZQUIERDA":"DERECHA","DERECHA":"IZQUIERDA"}
 class DummyTest:
     def __init__(self):
         self.valid_init_range =  4
         self.valid_end_range  =  8
         self.full_range = 10
-        self.dire = 0
+        self.dire = "DERECHA"
         self.cur_pos = 0
         self.cmd_pos = 0
         logging.getLogger().setLevel(logging.DEBUG)
         logging.basicConfig(format='%(asctime)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s')
 
     def move_motor_with_sensor(self,steps, dire):
-        logging.info("moving: %d  to dir(%d)" % (steps, dire))
+        logging.info("moving: %d  to dir(%s)" % (steps, dire))
 
     def get_valid_steps(self, current_position, step):
-        # set commanded position
-        if current_position == 0 :
-            cmd_position = self.valid_init_range + step
-            self.move_motor_with_sensor(cmd_position, self.dire)
-            return current_position, cmd_position
-        else:
-            cmd_position = current_position + step
-        # try to move acording borders
-        if cmd_position >= self.valid_init_range and cmd_position <= self.valid_end_range:
-            logging.info("valid cmd_pos : %d" % cmd_position)
-            current_position = cmd_position
-            self.move_motor_with_sensor(step, self.dire)
-        else:
-            logging.error("NOT VALID cmd_pos : %d" % cmd_position)
-            if cmd_position > self.valid_end_range:
-                cmd_position = self.valid_init_range
-                logging.error("back to init cmd_pos : %d" % cmd_position)
-                self.move_motor_with_sensor(cmd_position, self.dire)
-                current_position = cmd_position
-                logging.error("current pos: %d" % current_position)
-    
-            if cmd_position < self.valid_init_range:
-                cmd_position = self.valid_end_range
-                logging.error("back to end cmd_pos : %d" % cmd_position)
-                self.move_motor_with_sensor(cmd_position, self.dire)
-                current_position = cmd_position
-                logging.error("current pos: %d" % current_position)
+        logging.info("cur_pos : %d" % current_position)
+        cmd_position = current_position + step
+        logging.info("cmd_pos : %d" % cmd_position)
+        if cmd_position < self.valid_init_range:
+            logging.error("cmd from %d --> %d" % (cmd_position, self.valid_init_range))
+            cmd_position = self.valid_init_range
+        if cmd_position > self.valid_end_range:
+            logging.error("cmd from %d --> %d" % (cmd_position, self.valid_init_range))
+            cmd_position = self.valid_init_range
 
+        pasos = cmd_position - current_position
+        if pasos > 0:
+            logging.info("steps to cmd_pos: %d" % pasos)
+            self.move_motor_with_sensor(pasos, self.dire)
+        else:
+            logging.info("steps to cmd_pos: %d" % pasos)
+            self.dire = CAMBIO[self.dire]
+            pasos = abs(pasos)
+            self.move_motor_with_sensor(pasos, self.dire)
+            self.dire = CAMBIO[self.dire]
+
+        current_position = cmd_position
         return current_position, cmd_position
 
 if __name__ == '__main__':
