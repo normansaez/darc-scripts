f23b2509c0ccc7f475b60c7ed874dbdebf779fb9
Norman Saez <nsaez@alma.cl>
2013-08-13 00:43:32 -0400

    Modifications added to test (TTD)

--- BeagleDarc/BeagleDarc/Peripherals.py
+++ BeagleDarc/BeagleDarc/Peripherals.py
@@ -1,5 +1,91 @@
-class Motor:
-    pass
+import socket
 
-class Led:
-    pass
+'''
+Peripherals
+'''
+class Device:
+    '''
+    Common methods to communicate with BeagleBone
+    '''
+    def __init__(self):
+        self.pin = None
+        self.name = None
+        self.s = None
+        self.simulated = False
+
+    def connect(self):
+        '''
+        '''
+        self.s = socket.socket()         
+        host = socket.gethostname() 
+        port = 12345         
+        self.s.connect((host, port))
+
+    def disconnect(self):
+        '''
+        '''
+        self.s.close
+
+    def send_and_wait(self, msg):
+        self.connect()
+        self.s.send(msg)
+        while 1:
+            response = self.s.recv(1024)
+            #print response
+            if response.__contains__("ack"):
+                self.disconnect()
+                break
+
+class Motor(Device):
+    '''
+    Motor
+    '''
+    def __init__(self, name):
+        '''
+        Instanciate by name
+        '''
+        Device.__init__(self)
+        valid_names = ['ground_layer', 'horizontal_altitude_layer', 'vertical_altitude_layer']
+        if name in valid_names:
+            self.name = name
+            self.number = None
+            self.cur_pos = None
+            self.steps = None
+            self.direction = None
+            self.velocity = None
+            self.vr_init = None
+            self.vr_end = None
+        else:
+            msg = "Valid names are: ground_layer, horizontal_altitude_layer, vertical_altitude_layer , change: %s by a valid name" % name 
+            raise NameError(msg)
+
+class Led(Device):
+    '''
+    Led
+    '''
+    def __init__(self, number):
+        '''
+        Instanciate by number
+        '''
+        Device.__init__(self)
+        n_min = 0
+        n_max = 55
+        if number > n_min and number <= n_max:
+            self.number = number
+            self.exp_time = None
+            self.brightness = None
+        else:
+            msg = "Valid names are: %d to %d, change: %s by a valid name" % (n_min+1, n_max, number)
+            raise NameError(msg)
+
+    def set_on(self):
+        '''
+        ''' 
+        self.send_and_wait('turn on')
+        pass
+
+    def set_off(self):
+        '''
+        '''
+        pass
+
--- BeagleDarc/test/UnitTests.py
+++ BeagleDarc/test/UnitTests.py
@@ -25,31 +25,61 @@ class BBBTest(unittest.TestCase):
         self.assertNotEqual('No module named BeagleDarc', message)
         ##########################################################
         try:
-            from BeagleDarc import Motor
+            from BeagleDarc.Peripherals import Motor
         except Exception, e:
             message = e.message
-        self.assertNotEqual('cannot import name Motor', message)
+        self.assertNotEqual('cannot import name motor', message)
         ##########################################################
         try:
-            from BeagleDarc import Led
+            from BeagleDarc.Peripherals import Led
         except Exception, e:
             message = e.message
-        self.assertNotEqual('cannot import name Led', message)
+        self.assertNotEqual('cannot import name led', message)
 
     def test_intanciate_motor(self):
         '''
         '''
-        from BeagleDarc import Motor
+        from BeagleDarc.Peripherals import Motor
+        self.assertRaises(NameError,Motor,'dummy_name')
         m = Motor('ground_layer')
+        self.assertEqual (m.__class__.__name__,'Motor')
         m = Motor('horizontal_altitude_layer')
+        self.assertEqual (m.__class__.__name__,'Motor')
         m = Motor('vertical_altitude_layer')
+        self.assertEqual (m.__class__.__name__,'Motor')
+        ####################
+
+    def test_intanciate_led(self):
+        '''
+        '''
+        from BeagleDarc.Peripherals import Led
+        n_min = 0
+        n_max = 55
+
+        for led in range(0,66):
+            if led <= n_min:
+                self.assertRaises(NameError, Led, led)
+            elif led > n_max: 
+                self.assertRaises(NameError, Led, led)
+            else:
+                l = Led(led)
+                self.assertEqual (l.__class__.__name__,'Led')
+        ####################
+
+    def test_led_on(self):
+        '''
+        '''
+        from BeagleDarc.Peripherals import Led
+        l = Led(1)
+        self.assertEqual (l.__class__.__name__,'Led')
+        l.set_on()
         ####################
 
 if __name__ == '__main__':
-#    bbbt = unittest.TestLoader().loadTestsFromTestCase(BBBTest)
-    bbbt = unittest.TestSuite()
-    bbbt.addTest(BBBTest('test_import'))
-    bbbt.addTest(BBBTest('test_intanciate_motor'))
+    bbbt = unittest.TestLoader().loadTestsFromTestCase(BBBTest)
+#    bbbt = unittest.TestSuite()
+#    bbbt.addTest(BBBTest('test_import'))
+#    bbbt.addTest(BBBTest('test_intanciate_motor'))
 
     ##############################################################
     allsuites = unittest.TestSuite([bbbt])
