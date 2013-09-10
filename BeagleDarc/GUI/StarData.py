1afcdbeacad7d539d75f1408d34be0f255e878bc
Norman Saez <nsaez@alma.cl>
2013-09-10 20:24:35 -0300

    Adding buttons

--- BeagleDarc/GUI/StarData.py
+++ BeagleDarc/GUI/StarData.py
@@ -27,7 +27,6 @@ class StarData:
 
         ## star combo
         store = gtk.ListStore(gobject.TYPE_STRING)
-        #store = gtk.ListStore(str)
         for i in range(1,100):
             store.append([str(i)])
 
@@ -37,17 +36,83 @@ class StarData:
         self.combobox_star.set_text_column(0)
         #self.combobox_star.set_active(0)
 
-        #this could be added , but it seems unnecesary
+        #this could be added , but it seems to be unnecesary
         #cell = gtk.CellRendererText()
         #self.combobox_star.pack_start(cell, True)
         #self.combobox_star.add_attribute(cell, 'text', 0)
+    
+        #PIN
+        self.entry_pin = self.builder.get_object("entry_pin")
+        
+        #NAME
+        self.entry_name = self.builder.get_object("entry_name")
+        #SIM
+        self.checkbutton_sim = self.builder.get_object("checkbutton_sim")
+        
+        #EXP_T
+        store_exp = gtk.ListStore(gobject.TYPE_STRING)
+        secs = [x * 0.1 for x in range(0, 600)]
+        for i in secs:
+            store_exp.append([str(i)])
+        self.combobox_exp_time = self.builder.get_object("combobox_exp_time")
+        #self.combobox_exp_time.child.connect('changed', self.changed_cb)
+        self.combobox_exp_time.set_model(store_exp)
+        self.combobox_exp_time.set_text_column(0)
+        
+        #BRIGHT
+        store_bright = gtk.ListStore(gobject.TYPE_STRING)
+        for i in range(0,100):
+            store_bright.append([str(i)])
+        self.combobox_brightness = self.builder.get_object("combobox_brightness")
+        #self.combobox_brightness.child.connect('changed', self.changed_cb)
+        self.combobox_brightness.set_model(store_bright)
+        self.combobox_brightness.set_text_column(0)
+        
+        #IMG_PRE
+        self.entry_image_prefix = self.builder.get_object("entry_image_prefix")
+
+        #SAVE/RESTORE BUTTONS
+        self.button_default = self.builder.get_object("button_default")
+        self.button_save = self.builder.get_object("button_save")
+
+        dic = { 
+            "on_window1_destroy" : self.quit,
+            "on_button_save_clicked" : self.save,
+            'on_button_default_clicked'  : self.default
+        }
+        self.builder.connect_signals(dic)
+
+    def fill_info(self, star):
+        try:
+            config_name = 'led_%s' % star
+            self.entry_pin.set_text(self.model.get_star_pin(config_name))
+            self.entry_name.set_text(self.model.get_star_name(config_name))
+            simulated  = self.model.get_star_simulated(config_name)
+            if simulated is True:
+                self.checkbutton_sim.set_active(True)
+            self.combobox_exp_time.insert_text(0, str(self.model.get_star_exp_time(config_name))) 
+            self.combobox_exp_time.set_active(0)
+            self.combobox_brightness.insert_text(0, str(self.model.get_star_brightness(config_name))) 
+            self.combobox_brightness.set_active(0)
+            self.entry_image_prefix.set_text(self.model.get_star_image_prefix(config_name))
+        except Exception, e:
+            print e
+            #print "no section configured"
 
     def changed_cb(self, entry):
-        print 'Getting star: ', entry.get_text()
+        star = entry.get_text()
+        print 'Getting star: ', star
+        self.fill_info(star)
         return
 
     def quit(self, widget):
         sys.exit(0)
+
+    def save(self, widget):
+        print "saved"
+
+    def default(self, widget):
+        print "restored"
 
 
 if __name__ == '__main__':
