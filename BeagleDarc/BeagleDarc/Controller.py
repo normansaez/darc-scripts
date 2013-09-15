bda657d7347a767bfe225b3c4438b58da2d646f9
Norman Saez <nsaez@alma.cl>
2013-09-15 09:20:11 -0300

    Fixing controller

--- BeagleDarc/BeagleDarc/Controller.py
+++ BeagleDarc/BeagleDarc/Controller.py
@@ -3,8 +3,8 @@
 Controller
 '''
 
-from BeagleDarc.Peripherals  import Led
-from BeagleDarc.Model import Model
+from BeagleDarc.Model import Star
+
 class Controller:
     '''
     Controller:
@@ -12,15 +12,14 @@ class Controller:
     def __init__(self):
         '''
         '''
-        self.Led = Led
-        self.model = Model()
+        pass
 
     def turn_on(self, star):
-        l = self.Led(star)
+        l = Star(star)
         l.set_on()
 
     def turn_off(self, star):
-        l = self.Led(star)
+        l = Star(star)
         l.set_off()
 
 if __name__ == '__main__':
