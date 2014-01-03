ea1d631bfa367fc609aac304d0323f98db94e0c8
Norman Saez <nfsaez@uc.cl>
2014-01-03 01:47:28 -0300

    Adding controller corba stuff

--- BeagleDarc/BeagleDarc/Controller.py
+++ BeagleDarc/BeagleDarc/Controller.py
@@ -4,8 +4,11 @@ Controller
 '''
 
 from BeagleDarc.BeagleDarcClient import BeagleDarcClient
+from BeagleDarc.Model import BeagleDarcServerM 
 from BeagleDarc.Model import Star
 from BeagleDarc.Model import Layer
+
+import CORBA, BBBServer
 
 class Controller:
     '''
@@ -14,23 +17,29 @@ class Controller:
     def __init__(self):
         '''
         '''
-        #check server, partirlo remoto o local.
-        pass
+        orb = CORBA.ORB_init()
+        bds = BeagleDarcServerM('beagledarc_server')
+
+        self.cli_obj = orb.string_to_object(bds.ior)
+
     #star methods
     def star_on(self, star_id):
         star = Star(star_id)
-        pass
+        #self.cli_obj.led_on(star.name, star.pin_led, star.pin_pwm, star.pin_enable)
+        self.cli_obj.led_on(star.name, star.pin_led, star.pin_group, star.pin_enable)
+
+
     def star_off(self, star_id):
         star = Star(star_id)
-        pass
+#        self.cli_obj.led_off(name, pin_led, pin_pwm, pin_enable)
 
     #layer methods
     def layer_move(self, layer_id):
         layer = Layer(layer_id)
-        pass
+#        self.cli_obj.motor_move(name, steps, vel, pin_dir, pin_step, pin_sleep, stat_der, stat_izq)
+
     def layer_move_skip_sensor(self, layer_id):
         layer = Layer(layer_id)
-        pass
 
 if __name__ == '__main__':
     c = Controller()
